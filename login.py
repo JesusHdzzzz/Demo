a6e8c07ebec6f0bad258d7651f49d41a358da55b
isaac <jeekd3@gmail.com>
2025-03-08 14:20:45 -0800

    fixed button

--- login.py
+++ login.py
@@ -8,7 +8,7 @@ left, center, right = st.columns(3)
 with center:
     switch_login = st.button('Login')
     if switch_login:
-        st.switch_page("pages/login.py")
+        st.switch_page("pages/login_page.py")
 
     switch_signup = st.button('Sign Up')
     if switch_signup:
