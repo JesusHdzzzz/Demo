4496928b803179d1e6a32a3bdce2b8e3df5bd02a
isaac <jeekd3@gmail.com>
2025-03-08 12:39:40 -0800

    testing buttons

--- pages/login.py
+++ pages/login.py
@@ -2,3 +2,11 @@ import streamlit as st
 import os
 
 st.title("THE BEST BUDGETING APP")
+
+animal_shelter = ['cat', 'dog', 'rabbit', 'bird']
+
+animal = st.text_input('Type an animal')
+
+if st.button('Check availability'):
+    have_it = animal.lower() in animal_shelter
+    'We have that animal!' if have_it else 'We don\'t have that animal.'
