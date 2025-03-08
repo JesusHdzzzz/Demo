edacbb25c1779908e7808e20addf0a1c543d8ee8
isaac <jeekd3@gmail.com>
2025-03-08 14:20:15 -0800

    button goes to login page

--- login.py
+++ login.py
@@ -8,7 +8,7 @@ left, center, right = st.columns(3)
 with center:
     switch_login = st.button('Login')
     if switch_login:
-        st.switch_page("pages/page.py")
+        st.switch_page("pages/login.py")
 
     switch_signup = st.button('Sign Up')
     if switch_signup:
