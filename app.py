2fcc172b800b4bc87660e54accd870b80ee467a9
JesusHdzzzz <jesusantoniohernandezchairez@gmail.com>
2025-03-08 10:52:45 -0800

    testing input

--- app.py
+++ app.py
@@ -7,6 +7,6 @@ st.title("Testing input")
 prompt = st.text_input("Enter your prompt: ")
 
 if len(prompt) > 10: # Check if prompt:
-    st.write("Prompt is longre than 10 characters")
+    st.write("Prompt is longer than 10 characters")
 else:
     st.write("Prompt is shorter than 10 characters")
