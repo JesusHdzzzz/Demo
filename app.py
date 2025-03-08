915386dde41e86327055677f76b0f9ff2b2c4e56
JesusHdzzzz <jesusantoniohernandezchairez@gmail.com>
2025-03-08 01:45:46 -0800

    testing gemini API

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
+from google import genai
 import streamlit as st
+
+client = genai.Client(api_key="AIzaSyDwVFHhjS6X8wxoLmFgi7Y4oq2gPBEatMY")
 
 st.write("""
 # My first app
 I have deployed my first app using Streamlit*
 """)
+
+response = client.models.generate_content(
+    model="gemini-2.0-flash",
+    contents="Explain how AI works",
+)
+
+st.write(response.text)
