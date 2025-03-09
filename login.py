f5bf04b5c90dc9c690a7964179e92ff9c06ca8e9
isaac <jeekd3@gmail.com>
2025-03-08 20:22:33 -0800

    fixed homepage

--- login.py
+++ login.py
@@ -1,18 +1,20 @@
 import streamlit as st
 import os
 
-left, center, right = st.columns([2, 2, 1], vertical_alignment="bottom", border=True)
-
 style_heading = 'text-align: center'
 style_image = 'display: block; margin-left: auto; margin-right: auto; margin-top: 100px' 
 
-with st.container():
-    st.markdown(f"<h1 style='{style_heading}'>Bobcat Budgeting</h1>", unsafe_allow_html=True)
+st.markdown(f"<h1 style='{style_heading}'>Bobcat Budgeting</h1>", unsafe_allow_html=True)
 
-    switch_login = st.button('Login')
-    if switch_login:
-        st.switch_page("pages/login_page.py")
+left, center, right = st.columns([1.8, 1.5, 1.9], vertical_alignment="bottom")
 
-    switch_signup = st.button('Sign Up')
-    if switch_signup:
-        st.switch_page("pages/signup_page.py")
+with center:
+    colLeft, colRight = st.columns(2, vertical_alignment="bottom")
+    with colLeft:
+        switch_login = st.button('Login', type="primary")
+        if switch_login:
+            st.switch_page("pages/login_page.py")
+    with colRight:
+        switch_signup = st.button('Sign Up', type="primary")
+        if switch_signup:
+            st.switch_page("pages/signup_page.py")
