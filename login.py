62373bdf7efe29b384e2eefd049e621fee123100
ricecakes119 <jasonelcano119@gmail.com>
2025-03-08 20:44:08 -0800

    sidebar chat

--- login.py
+++ login.py
@@ -9,10 +9,13 @@ style_image = 'display: block; margin-left: auto; margin-right: auto; margin-top
 with st.container():
     st.markdown(f"<h1 style='{style_heading}'>Bobcat Budgeting</h1>", unsafe_allow_html=True)
 
-    switch_login = st.button('Login')
-    if switch_login:
-        st.switch_page("pages/login_page.py")
+    col1, col2 = st.columns(2)
+    with col1:
+        switch_login = st.button('Login')
 
-    switch_signup = st.button('Sign Up')
+    with col2:
+        switch_signup = st.button('Sign Up')
     if switch_signup:
         st.switch_page("pages/signup_page.py")
+    if switch_login:
+        st.switch_page("pages/login_page.py")
--- pages/app.py
+++ pages/app.py
@@ -13,28 +13,29 @@ else:
     model = genai.GenerativeModel('gemini-1.5-flash', system_instruction="Only answer questions about finance, refuse unrelated questions.")
 
 st.title("ChatAI")
+with st.sidebar:
+    st.header("Chat with AI")
+    # Initialize messages
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
 
-# Initialize messages
-if "messages" not in st.session_state:
-    st.session_state.messages = []
+    for msg in st.session_state.messages:
+        with st.chat_message(msg["role"]):
+            st.write(msg["content"])
 
-for msg in st.session_state.messages:
-    with st.chat_message(msg["role"]):
-        st.write(msg["content"])
+    prompt = st.chat_input("Ask a finance question: ")
 
-prompt = st.chat_input("Ask a finance question: ")
-
-if prompt:
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    with st.chat_message("user"):
-        st.write(prompt)
-    try:
-        response = model.generate_content(prompt)
-        st.session_state.messages.append({"role": "bot", "content": response.text})
-        with st.chat_message("bot"):
-            st.write(response.text)
-    except Exception as e:
-        st.error(f"An error occurred: {e}")
+    if prompt:
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        with st.chat_message("user"):
+            st.write(prompt)
+        try:
+            response = model.generate_content(prompt)
+            st.session_state.messages.append({"role": "bot", "content": response.text})
+            with st.chat_message("bot"):
+                st.write(response.text)
+        except Exception as e:
+            st.error(f"An error occurred: {e}")
 
 # if len(prompt) > 10: # Check if prompt:
 #     # st.write("Prompt is longer than 10 characters")
