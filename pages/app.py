4b00539187a20f355bca2a274b3b7f42b18c8112
ricecakes119 <jasonelcano119@gmail.com>
2025-03-08 16:07:08 -0800

    AI is now chatbox

--- pages/app.py
+++ pages/app.py
@@ -10,16 +10,36 @@ else:
     genai.configure(api_key=api_key)
     model = genai.GenerativeModel('gemini-1.5-flash', system_instruction="Only answer questions about finance, refuse unrelated questions.")
 
-st.title("Testing inputs")
+st.title("ChatAI")
 
-prompt = st.text_input("Enter your prompt: ")
+# Initialize messages
+if "messages" not in st.session_state:
+    st.session_state.messages = []
 
-if len(prompt) > 10: # Check if prompt:
-    # st.write("Prompt is longer than 10 characters")
+for msg in st.session_state.messages:
+    with st.chat_message(msg["role"]):
+        st.write(msg["content"])
+
+prompt = st.chat_input("Ask a finance question: ")
+
+if prompt:
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.write(prompt)
     try:
         response = model.generate_content(prompt)
-        st.write(response.text)
+        st.session_state.messages.append({"role": "bot", "content": response.text})
+        with st.chat_message("bot"):
+            st.write(response.text)
     except Exception as e:
         st.error(f"An error occurred: {e}")
-else:
-    st.write("Prompt is shorter than 10 characters")
+
+# if len(prompt) > 10: # Check if prompt:
+#     # st.write("Prompt is longer than 10 characters")
+#     try:
+#         response = model.generate_content(prompt)
+#         st.write(response.text)
+#     except Exception as e:
+#         st.error(f"An error occurred: {e}")
+# else:
+#     st.write("Prompt is shorter than 10 characters")
--- pages/login_page.py
+++ pages/login_page.py
@@ -1,9 +1,32 @@
 import streamlit as st
+import sqlite3
+
+conn = sqlite3.connect('../data.db')
+c = conn.cursor()
+
+
+c.execute('''Create TABLE IF NOT EXISTS users (id TEXT PRIMARY KEY, usertype TEXT, username TEXT, password TEXT)''')
+conn.commit()
+
+c.execute("INSERT INTO users (id, usertype, username, password) VALUES ('1', 'admin', 'admin', 'password')")
+conn.commit()
 
 st.title("Login")
 
 email = st.text_input("Email")
-st.write(email)
+# st.write(email)
 
-password = st.text_input("Password")
-st.write(password)
+password = st.text_input("Password", type="password")
+# st.write(password)
+
+
+if st.button("Login"):
+    c.execute("SELECT * FROM users WHERE username = ? AND password = ?", (email, password))
+    user = c.fetchone()
+
+    if user:
+        st.success("Login Successful")
+    else:
+        st.error("Invalid Email or Password")
+
+conn.close()
