f933788cacecab905895810ebde82d2ab7cd2a39
isaac <jeekd3@gmail.com>
2025-03-08 20:37:19 -0800

    fixed homepage

--- login.py
+++ login.py
@@ -1,12 +1,22 @@
 import streamlit as st
 import os
 
-style_heading = 'text-align: center'
-style_image = 'display: block; margin-left: auto; margin-right: auto; margin-top: 100px' 
+style_heading = 'text-align: center; font-size:80px'
+
+st.markdown("\n")
+st.markdown("\n")
+st.markdown("\n")
+st.markdown("\n")
+st.markdown("\n")
+st.markdown("\n")
+st.markdown("\n")
 
 st.markdown(f"<h1 style='{style_heading}'>Bobcat Budgeting</h1>", unsafe_allow_html=True)
 
-left, center, right = st.columns([1.8, 1.5, 1.9], vertical_alignment="bottom")
+st.markdown("\n")
+st.markdown("\n")
+
+left, center, right = st.columns([1.8, 2, 1.9], vertical_alignment="bottom")
 
 with center:
     colLeft, colRight = st.columns(2, vertical_alignment="bottom")
